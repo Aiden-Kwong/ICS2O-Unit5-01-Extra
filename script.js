4f5f3f364750a3a2d3eeb045c18a2d43d7dcaf3e
AidenKwong <>
2021-06-01 13:55:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let x = 0;
+
+function myFunction() {
+  document.getElementById("change").innerHTML = "This message was changed with a script!";
+}
+
+function changeColor() {
+  document.getElementById("color").style.color = "#49111c"
+}
+
+function buttonClicks() {
+  x++;
+  if (x == 5)
+  document.getElementById("varchange").innerHTML = "This message changed after 5 clicks on the button!"
+}
