ab23c33c9277a6e702e711e96a83fc2666d6b06f
AidenKwong <>
2021-06-01 14:12:17 +0000

    partial linter fix

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let x = 0
 
-function myFunction() {
+function myFunction () {
   document.getElementById('change').innerHTML = 'This message was changed with a script!'
 }
 
-function changeColor() {
+function changeColor () {
   document.getElementById('color').style.color = '#49111c'
 }
 
-function buttonClicks() {
+function buttonClicks () {
   x++
-  if (x == 5)
-  document.getElementById('varchange').innerHTML = 'This message changed after 5 clicks on the button!'
+  if (x === 5) {
+    document.getElementById('varchange').innerHTML = 'This message changed after 5 clicks on the button!'
+  }
 }
