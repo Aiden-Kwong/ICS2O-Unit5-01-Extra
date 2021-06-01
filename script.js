5dac5a44938ab5f7ca2badcc67c1417444f6d174
AidenKwong <>
2021-06-01 14:03:26 +0000

    Linter fix?

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let x = 0;
+let x = 0
 
 function myFunction() {
-  document.getElementById("change").innerHTML = "This message was changed with a script!";
+  document.getElementById('change').innerHTML = 'This message was changed with a script!'
 }
 
 function changeColor() {
-  document.getElementById("color").style.color = "#49111c"
+  document.getElementById('color').style.color = '#49111c'
 }
 
 function buttonClicks() {
-  x++;
+  x++
   if (x == 5)
-  document.getElementById("varchange").innerHTML = "This message changed after 5 clicks on the button!"
+  document.getElementById('varchange').innerHTML = 'This message changed after 5 clicks on the button!'
 }
